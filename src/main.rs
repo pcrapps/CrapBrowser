a80fd9ac0c0bda8e2273600528c19ccba52eebff
Patrick Crapps <patrickcrapps@gmail.com>
2025-03-07 09:36:13 -0600

    adding first mods

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod fetch;
+mod parser;
+mod render;
+
+fn main() {
+    //let url = "https://www.rust-lang.org/";
+    //let html = fetch::fetch(url);
+    // let dom = parser::parse(html);
+    // let rendered = render::render(dom);
+    // println!("{}", rendered);
+    println!("Hello, world!");
+}
